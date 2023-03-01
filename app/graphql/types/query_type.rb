e663024cf90feeea3088f7986a988d0b74cf4f20
Luis Roberto <luisrobertopenav@gmail.com>
2023-03-01 10:09:30 -0700

    remove boiler-plate code

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,23 +1,5 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-  end
-end
-
-module Types
-  class QueryType < Types::BaseObject
     field :users, [Types::UserType], null: false
     def users
       User.all
