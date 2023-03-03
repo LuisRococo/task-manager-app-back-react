4aa69e7f26c74f2f6b7839e3c445aff05735cbf2
Luis Roberto <luisrobertopenav@gmail.com>
2023-03-03 11:08:30 -0700

    add 'edit task through graphql' feature

---
+++ app/graphql/mutations/patch_task.rb
@@ -0,0 +1,27 @@
+module Mutations
+    class PatchTask < BaseMutation
+      # arguments passed to the `resolve` method
+      argument :id, Integer, required: true
+      argument :title, String, required: false
+      argument :description, String, required: false
+      argument :points, Integer, required: false
+      argument :taskListId, Integer, required: false
+      argument :order, Integer, required: false
+      argument :completed, Boolean, required: false
+  
+      # return type from the mutation
+      type Types::TaskType
+  
+      def resolve(id:, title:, description:, points:, taskListId:, order:, completed:)
+        task_to_update = Task.find(id)
+        task_to_update.title = title if title
+        task_to_update.description = description if description
+        task_to_update.points = points if points
+        task_to_update.task_list_id = taskListId if taskListId
+        task_to_update.order = order if order
+        task_to_update.completed = completed if completed
+        task_to_update.save!
+        task_to_update
+      end
+    end
+  end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :delete_task_list, mutation: Mutations::DeleteTaskList
     field :patch_task_list, mutation: Mutations::PatchTaskList
     field :create_task, mutation: Mutations::CreateTask
+    field :patch_task, mutation: Mutations::PatchTask
   end
 end
